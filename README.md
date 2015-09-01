a53499f30cb95f6eba310c1d5aac7231a697fcb3
daniel.plominski <daniel@plominski.eu>
2015-09-01 15:58:04 +0200

    Version: 0.1.1.1

---
+++ README.md
@@ -0,0 +1,31 @@
+
+Background
+==========
+
+WARNING
+=======
+
+Dependencies
+============
+* MacOS
+   * [brew](http://brew.sh/)
+   * [brew/rsync](http://brew.sh/)
+
+Features
+========
+* MacOS
+
+Platform
+========
+* Mac OS X 10.5+
+
+Usage
+=====
+    # ./mac_cifs_rsync.sh -s {local source} -i {remote ip} -p {remote path} -m {local mountpoint} -u {user} -pw {password}
+
+Screencast
+==========
+
+Errata
+======
+
---
+++ mac_cifs_rsync.sh
@@ -0,0 +1,229 @@
+#!/bin/sh
+
+### LICENSE (BSD 2-Clause) // ###
+#
+# Copyright (c) 2014, Daniel Plominski (Plominski IT Consulting)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+### // LICENSE (BSD 2-Clause) ###
+
+### ### ### PLITC ### ### ###
+
+### ### ### ### ### ### ### ### ###   ### ### ### ### ### ### ### ### ###
+### # CONFIGURATION VARIABLES # ###   ### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ###   ### ### ### ### ### ### ### ### ###
+
+
+
+### ### ### ### ### ### ### ### ###   ### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ###   ### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ###   ### ### ### ### ### ### ### ### ###
+
+### stage0 // ###
+UNAME=$(uname)
+MYNAME=$(whoami)
+#
+PRG="$0"
+##/ need this for relative symlinks
+   while [ -h "$PRG" ] ;
+   do
+         PRG=$(readlink "$PRG")
+   done
+DIR=$(dirname "$PRG")
+#
+#/ spinner
+spinner()
+{
+   local pid=$1
+   local delay=0.01
+   local spinstr='|/-\'
+   while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+         local temp=${spinstr#?}
+         printf " [%c]  " "$spinstr"
+         local spinstr=$temp${spinstr%"$temp"}
+         sleep $delay
+         printf "\b\b\b\b\b\b"
+   done
+   printf "    \b\b\b\b"
+}
+### // stage0 ###
+
+### stage1 // ###
+case $UNAME in
+Darwin)
+   ### MacOS ###
+OSXVERSION=$(sw_vers -productVersion)
+BREW=$(/usr/bin/which brew)
+MRSYNC=$(/usr/bin/find /usr/bin /usr/local/bin -name "rsync" | grep -c "rsync")
+LASTUSER=$(/usr/bin/last | head -n 1 | awk '{print $1}')
+
+#/ LASTGROUP=$(/usr/bin/id "$LASTUSER" | grep -o 'gid=[^(]*[^)]*)' | sed 's/[0-9]//g' | sed 's/gid=(//g' | sed 's/)//g')
+##/ for OS X Yosemite
+LASTGROUP=$(/usr/bin/id "$LASTUSER" | awk '{print $2}' | sed 's/gid=//g')
+### ### ### ### ### ### ### ### ###
+
+if [ "$MYNAME" = root ]
+then
+   : # dummy
+else
+   echo "<--- --- --->"
+   echo ""
+   echo "ERROR: You must be root to run this script"
+   exit 1
+fi
+
+if [ -z "$BREW" ]
+then
+   echo "<--- --- --->"
+   echo "need homebrew"
+   echo "<--- --- --->"
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+   echo "<--- --- --->"
+fi
+
+if [ "$MRSYNC" = "1" ]
+then
+   echo "<--- --- --->"
+   echo "need rsync"
+   echo "<--- --- --->"
+        /usr/sbin/chown -R "$LASTUSER:$LASTGROUP" /usr/local
+        /usr/sbin/chown -R "$LASTUSER:$LASTGROUP" /Library/Caches/Homebrew
+        sudo -u "$LASTUSER" -s "/usr/local/bin/brew install rsync"
+   echo "<--- --- --->"
+fi
+
+(
+##/ clean up
+/bin/rm -rf /tmp/mac_cifs_rsync*.txt
+)
+
+### stage2 // ###
+
+### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ###
+
+while getopts ":s:i:p:m:u:pw:" opt; do
+  case "$opt" in
+    s) source=$OPTARG ;;
+    i) ip=$OPTARG ;;
+    p) path=$OPTARG ;;
+    m) mountpoint=$OPTARG ;;
+    u) user=$OPTARG ;;
+    pw) password=$OPTARG ;;
+  esac
+done
+shift $(( OPTIND - 1 ))
+
+##/ check source - empty argument
+if [ -z "$source" ]
+then
+   echo "" # dummy
+   echo "usage:   ./mac_cifs_rsync.sh -s {local source} -i {remote ip} -p {remote path} -m {local mountpoint} -u {user} -pw {password}"
+   echo "example: -s /Users -i 192.168.1.1 -p server/rpool -m /freenas -u root -pw 123"
+   echo "" # dummy
+   exit 1
+fi
+
+##/ check ip - empty argument
+if [ -z "$ip" ]
+then
+   echo "" # dummy
+   echo "usage:   ./mac_cifs_rsync.sh -s {local source} -i {remote ip} -p {remote path} -m {local mountpoint} -u {user} -pw {password}"
+   echo "example: -s /Users -i 192.168.1.1 -p server/rpool -m /freenas -u root -pw 123"
+   echo "" # dummy
+   exit 1
+fi
+
+##/ check path - empty argument
+if [ -z "$path" ]
+then
+   echo "" # dummy
+   echo "usage:   ./mac_cifs_rsync.sh -s {local source} -i {remote ip} -p {remote path} -m {local mountpoint} -u {user} -pw {password}"
+   echo "example: -s /Users -i 192.168.1.1 -p server/rpool -m /freenas -u root -pw 123"
+   echo "" # dummy
+   exit 1
+fi
+
+##/ check mountpoint - empty argument
+if [ -z "$mountpoint" ]
+then
+   echo "" # dummy
+   echo "usage:   ./mac_cifs_rsync.sh -s {local source} -i {remote ip} -p {remote path} -m {local mountpoint} -u {user} -pw {password}"
+   echo "example: -s /Users -i 192.168.1.1 -p server/rpool -m /freenas -u root -pw 123"
+   echo "" # dummy
+   exit 1
+fi
+
+##/ check user - empty argument
+if [ -z "$user" ]
+then
+   echo "" # dummy
+   echo "usage:   ./mac_cifs_rsync.sh -s {local source} -i {remote ip} -p {remote path} -m {local mountpoint} -u {user} -pw {password}"
+   echo "example: -s /Users -i 192.168.1.1 -p server/rpool -m /freenas -u root -pw 123"
+   echo "" # dummy
+   exit 1
+fi
+
+##/ check password - empty argument
+if [ -z "$password" ]
+then
+   echo "" # dummy
+   echo "usage:   ./mac_cifs_rsync.sh -s {local source} -i {remote ip} -p {remote path} -m {local mountpoint} -u {user} -pw {password}"
+   echo "example: -s /Users -i 192.168.1.1 -p server/rpool -m /freenas -u root -pw 123"
+   echo "" # dummy
+   exit 1
+fi
+
+#/ check ip - numeric
+cip="$(echo "$ip" | sed 's/[^0-9,^.,^:,]*//g')"
+if [ "$cip" != "$ip" ]
+then
+   echo "" # dummy
+   echo "[ERROR] string -ip '"$ip"' has characters which are not numeric"
+   echo "" # dummy
+   exit 1
+fi
+
+### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ###
+
+### // stage2 ###
+;;
+*)
+    # error 1
+    echo "<--- --- --->"
+    echo ""
+    echo "ERROR: Plattform = unknown"
+    exit 1
+    ;;
+esac
+
+#
+### // stage1 ###
+
+exit 0
+### ### ### PLITC ### ### ###
+# EOF
